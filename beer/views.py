06195ffb83436db67c1fd1ce88d54d5da8b648fb
Alexandre Blanchet <alexandreblanchet08@gmail.com>
2019-11-01 15:41:30 +0100

    version 1

--- beer/views.py
+++ beer/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+  import json
+  import requests
+
+  if request.method == "POST":
+    zipcode = request.POST['zipcode']
+   
+    return render(request, 'home.html', {'api' : 'toto'})
+ 
+  return render(request, 'home.html', {})
 
 def about(request):
     return render(request, 'about.html', {})
